49c519cc27e73b90b138b406f3fdaeb87189e723
LEO VITOR DE CAMPOS <180976@facens.br>
2019-08-26 11:12:04 -0300

    First Commit

---
+++ kNN_Leo/kNN_Leo/Flor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kNN_Leo
+{
+    class Flor
+    {
+        public string type;
+        public Vector2 sepala;
+        public Vector2 petala;
+
+        public Flor(string p_type, Vector2 p_sepala, Vector2 p_petala)
+        {
+            type = p_type;
+            sepala = p_sepala;
+            petala = p_petala;
+        }
+    }
+}
+
+public class Vector2
+{
+    public float x;
+    public float y;
+
+    public Vector2(float argx, float argy)
+    {
+        x = argx;
+        y = argy;
+    }
+}
---
+++ kNN_Leo/kNN_Leo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace kNN_Leo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            float[] array = { 1.2f, 3.3f, 5.4f, 1.4f, 7.2f };
+
+            array = TimSort.timSort(array, array.Length);
+            //TimSort.printArray(array, array.Length);
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\180976\Desktop\iris.txt");
+
+            List<Flor> flores = new List<Flor>();
+
+            for(int i = 0; i < lines.Length; i++)
+            {
+                string[] aux;
+                aux = lines[i].Split(',');
+                Vector2 sepala = new Vector2(float.Parse(aux[0]), float.Parse(aux[1]));
+                Vector2 petala = new Vector2(float.Parse(aux[2]), float.Parse(aux[3]));
+                string type = aux[4];
+
+                flores.Add(new Flor(type, sepala, petala));
+            }
+
+            do
+            {
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
